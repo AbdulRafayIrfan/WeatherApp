ccd4be310f61264012ed26e5bbfb076204e5bf03
AbdulRafayIrfan <rafay.irfen@gmail.com>
2023-06-02 19:40:04 +0300

    Using backend deployed on Render

--- src/api/Weather.js
+++ src/api/Weather.js
@@ -1,24 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
 function getForecast(coords, units) {
-    return axios.get(`https://weather-app-ios.herokuapp.com/forecast?lat=${coords.latitude}&lon=${coords.longitude}&units=${units}`);
+  return axios.get(`https://ios-weather-app.onrender.com/forecast?lat=${coords.latitude}&lon=${coords.longitude}&units=${units}`);
 }
 
 function getCurrent(coords, units) {
-    return axios.get(`https://weather-app-ios.herokuapp.com/current?lat=${coords.latitude}&lon=${coords.longitude}&units=${units}`);
+  return axios.get(`https://ios-weather-app.onrender.com/current?lat=${coords.latitude}&lon=${coords.longitude}&units=${units}`);
 }
 
 function getPartOfDayStyle(current) {
-    let sunrise, sunset, now;
-    sunrise = current.sys.sunrise;
-    sunset = current.sys.sunset;
-    now = (Date.now() / 1000).toFixed(0);
-    // Check if sunrise / sunset
-    if (now < sunrise || now > sunset) {
-        return "linear-gradient(49deg, rgba(62,54,69,1) 0%, rgba(18,0,25,1) 100%)";
-    } else {
-        return "linear-gradient(58deg, rgba(201,161,231,1) 0%, rgba(0,212,255,1) 100%)";
-    }
+  const { sunrise } = current.sys;
+  const { sunset } = current.sys;
+  const now = (Date.now() / 1000).toFixed(0);
+  // Check if sunrise / sunset
+  if (now < sunrise || now > sunset) {
+    return 'linear-gradient(49deg, rgba(62,54,69,1) 0%, rgba(18,0,25,1) 100%)';
+  }
+  return 'linear-gradient(58deg, rgba(201,161,231,1) 0%, rgba(0,212,255,1) 100%)';
 }
 
-export {getCurrent, getForecast, getPartOfDayStyle};
+export { getCurrent, getForecast, getPartOfDayStyle };
